8b8701089c3afa5dcae6b10f33f6386a0c733b19
joshjames123 <bueranoj@gmail.com>
2022-05-18 11:30:16 +0800

    connected to database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //[SECTION] Dependencies and Modules
 	const express = require('express');
+	const mongoose = require('mongoose');
 
 //[SECTION] Environment Setup
 	const port = 4000;
@@ -8,7 +9,15 @@
 	const app = express();
 
 //[SECTION] Database Connection
+	mongoose.connect('mongodb+srv://joshjames123:admin123@cluster0.ldn4y.mongodb.net/?retryWrites=true&w=majority')
+	//check the connection status
+	const connectStatus = mongoose.connection
+	//lets checj if the connection emitted an 'open' event.
+	connectStatus.once('open', () => console.log(`Database Connected`));
+
 //[SECTION] Backend Routes
+	
+
 //[SECTION] Server Gateway Respose
 	app.listen(port, () => {
 		console.log(`API is Hosted port ${port}`);
