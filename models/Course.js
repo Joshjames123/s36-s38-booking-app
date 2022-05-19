7c2f03c824a6ecd6242372926dfb4301f7c80d88
joshjames123 <bueranoj@gmail.com>
2022-05-19 11:52:03 +0800

    cleaned house

--- models/Course.js
+++ models/Course.js
@@ -2,13 +2,7 @@
 	const mongoose = required('mongoose');
 
 //[SECTION] Schema/Blueprint
-	//1. identufy what each course in the collection would look like.
-		//-> name,
-		//-> description
-		//-> price
-		//-> isActive (determin wether the subject is offerd)
-		//-> createdOn (when the subject was included in the database)
-		//-> enrollees (students enrolled in each subject)
+
 	const courseSchema = new mongoose.Schema({
 		name {
 			type: String,
@@ -25,14 +19,13 @@
 		isActive {
 			type: Boolean,
 			default: true
-		}, //this is to identify if the subject is currently being offered by the institution.
+		},
 		createdOn {
 			type: Date,
 			default: new Date()
-		}, //'new Date()' expression instantiates a new date stores the current date and time whenever a course is created in the database.
+		},
 		enrollees: [
 			{
-				//each object stored in this array would identify a single student
 				userID: {
 					type: String,
 					required: [true, 'Student ID is Required']
@@ -46,5 +39,4 @@
 	});
 
 //[SECTION] Model
-	//lets produce a model which will be used as interface between the server side app and the database.
 	module.exports = mongoose.model('Course', courseSchema)
--- models/User.js
+++ models/User.js
@@ -2,14 +2,7 @@
 	const mongoose = require('mogoose');
 
 // [SECTION] Schema/Blueprint
-	//1.Create an instance of a schema to describe the user Data
-		//-> first name
-		//-> last name
-		//-> email address
-		//-> password
-		//-> mobileNo
-		//-> isAdmin (to identify the role on the app)
-		//-> enrollments (list of subjects)
+
 	const userSchema = new mongoose.Schema({
 		firstName: {
 			type: String,
@@ -37,7 +30,6 @@
 		},
 		enrollments: [
 			{
-				//every document that will be stored in this array structure would describe a course/subject that a user is enrolled in.
 				courseID: {
 					type: String,
 					required: [true, 'Subject ID is Required']
@@ -45,18 +37,14 @@
 				enrolledON: {
 					type: Date,
 					default: new Date()
-				}, //date when the student enrolled
+				},
 				status: {
 					type: String,
 					default: 'Enrolled'
-				} //status of enrollemnt
+				}
 			}
 		]
 	})
 
 // [SECTION] Model
-	//create a model for the user that will be use as interface for the app to communicate with the Database Management System.
-	//SYNTAX: library.model(<Model Name>,<Schema>)
-	//Model Name -. will be processed to preduce the name of the collection, by converting all the character in all lowecase and adding 'S' at the end. 'users' => collection name result.
-	//expose the data so that it will become usable for the rest of the app.
 	module.exports = mongoose.model('User', userSchema);
