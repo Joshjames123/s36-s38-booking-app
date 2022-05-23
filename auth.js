8028552e607b5ae6a9c5375d62e3a2eb0adc3ce3
joshjames123 <bueranoj@gmail.com>
2022-05-23 13:02:06 +0800

    cleaned house

--- auth.js
+++ auth.js
@@ -1,74 +1,52 @@
 const jwt = require('jsonwebtoken');
-//User defined string data that will be used to create our JSON web tokens
-//Used in the algorithm for encrypting our data which makes it difficult to decode the information without the defined secret keyword
 const secret = 'CourseBookingAPI';
-
-
-//JWT is a way to securely pass information from one part of a server to the frontend or other parts of our application. This will allow us to authorize our users to access or disallow access to certain parts of our app.
-
 
 
 
 //Token Creation
-// Analogy: Pack the gift and provide a lock with the secret code as the key
 
 module.exports.createAccessToken = (user) =>{
-	//check if we can receive the details of the user from our login
+
 	console.log(user);
 
-	//object to contain some details of our user
-	//this will the data within our payload
 	const data = {
 		id: user._id,
 		email: user.email,
 		isAdmin: user.isAdmin
 	}
 
-	//Generate a JSON web token using the jwt's sign method
 	return jwt.sign(data, secret, {})
-
 
 }
 
 
 //Token Verification
-// Analogy: Receive the gift and open the lock to verify if the sender is legitimate and the gift was not tampered with.
 
 module.exports.verify = (req, res, next) => {
-	// the token is retrieved from the request header
+
 	console.log(req.headers.authorization)
 
 	let token = req.headers.authorization;
 
-	//This if statement will first check if the token variable contains undefined or a proper jwt. If its undefined, we will check token's data type with typeof, then send a message to the client.
 	if(typeof token === "undefined"){
 		return res.send({ auth: "Failed. No token" });
 	} else {
 		console.log(token);
-		//Bearer eyJsdkfjdskfnkdsjdslkfjdskf
-		//slice(<startingPosition>, <endPosition> )
 		token = token.slice(7, token.length)
 
-		//Validate the token using the "verify" method decrypting the token using the secret code
 		jwt.verify(token, secret, function(err, decodedToken) {
 
-				//err will contain the error from decoding your token. This will contain the reason why we will reject the token.
-
-				//If verification of the token is success, then jwt.verify will return the decoded token.
 			if(err){
 				return res.send({
 					auth: "Failed",
 					message: err.message
 				})
 			} else {
-				console.log(decodedToken); //contains the data from our token
+				console.log(decodedToken);
 				req.user = decodedToken
-				//user property will be added to request object and will contain our decodedToken. It can be accessed in the verify
-
 
 				next();
-				//middleware function
-				//next() will let us proceed to the next middleware Or controller
+
 			}
 		})
 
--- controller/users.js
+++ controller/users.js
@@ -38,29 +38,19 @@
 
 
 //User Authentication
-/*
-Stepts:
-1. Check the database if the user email exists
-2. Compare the password provided in the login from with password stored in the data base.
-3. Generate/return a JSON web token if the user is successfully logged in and return flase if not.
-
-*/
 
 module.exports.loginUser = (data) => {
-	//findOne method returns the first record in the collection that matches the search criteria
+
 	return User.findOne({ email: data.email }).then(result => {
 		//User does not exist
 		if(result == null) {
 			return false;
 		} else {
-			//User exists
-			//'compareSync' method from the bycrypt to used in comparing the non encrypted password from the login and the database password. it return 'true or false'
 			const isPasswordCorrect = bcrypt.compareSync(data.password, result.password)
 
 			if(isPasswordCorrect){
 				return {accessToken: auth.createAccessToken(result.toObject()) }
 			} else {
-				//password do not match
 				return false;
 			}
 		}
@@ -70,15 +60,8 @@ module.exports.loginUser = (data) => {
 
 //[SECTIONS] Functionalities [RETRIVE] the user details
 
-/*
-1. Find the document in the database using the user's ID.
-2. Reassign the password of the returned document to an empty string.
-3. return the result back to the client.
-*/
-
 module.exports.getProfile = (data) => {
 	return User.findById(data).then(result => {
-		//change the value of the users password to an empty string
 		result.password = '';
 		return result;
 	})
